7b5d4904dee788e04d791ceb68ee14812c45994e
gbondjin2000 <144464829+gbondjin2000@users.noreply.github.com>
2023-12-08 11:29:07 -0500

    Add files via upload

---
+++ gym.js
@@ -0,0 +1,6 @@
+document.getElementById('Membership-Form').addEventListener("submit",function(event){
+    var name = document.getElementById('Full-Name').value;
+    alert('Thank you '+ name+ ' your membership has been submitted');
+
+});
+
